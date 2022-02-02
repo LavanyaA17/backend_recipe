29dc94c0f0a57d0633ac59e8257de4ee387c4f10
LavanyaA17 <a.lavanya17@gmail.com>
2022-02-02 13:18:03 +0100

    backend details

---
+++ server.js
@@ -0,0 +1,50 @@
+
+import express from "express";
+//import { connect } from "mongoose";
+import mongoose from 'mongoose';
+import 'dotenv/config';
+//require("dotenv").config();
+import Recipe from "./models/recipeSchema.js";
+import cors from "cors";
+
+//import mongoose from 'mongoose';
+const app = express();
+app.use(cors());
+const port = 3020;
+app.use(express.json());
+
+mongoose
+.connect(process.env.MONGODB_URI)
+.then((e) => console.log("Connected to the databse", e))
+.catch((e) => console.log("Error", e));
+
+app
+.route("/")
+.get((req, res) => {
+    Recipe.find({})
+    .then((result) => res.send(result))
+    .catch((e) => 
+    {
+     console.log(e);
+    res.send({ error: true});
+});
+})
+.post((req, res) => {
+    console.log(req.body);
+    Recipe.create(req.body)
+    .then((result) => res.send(result))
+    .catch((e) => 
+    {
+     console.log(e);
+    res.send({ error: true});
+    });
+})
+
+
+.put((req, res) => {})
+.delete((req, res) => {});
+
+
+app.listen(port, () => {
+    console.log('Started server on port 3000')
+});
